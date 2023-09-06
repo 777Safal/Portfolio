8d9b88ecaa66776ca7cdd5ab4eb90760b68a4f32
777Safal <tsafal06@gmail.com>
2023-09-06 10:38:46 +0545

    new code

---
+++ src/Practice/Motion.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+function Motion() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <motion.h1 className='p-5'
+        initial={{scale:10}}
+        animate={{scale:1}}
+        transition={{duration:5}}
+        >Frmaer Motion</motion.h1>
+        <motion.h2 className="p-5"
+        whileHover={{scale:2 ,y:-10}}
+        >Animation</motion.h2>
+    </div>
+  )
+}
+
+export default Motion
